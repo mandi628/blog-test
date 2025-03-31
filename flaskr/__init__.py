d2a51998af5d88e2e2d44bb604907561074f078c
mandi628 <mandi628.codes@gmail.com>
2025-03-30 23:31:15 -0400

    Added CSS & templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app():
+def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -12,7 +12,7 @@ def create_app():
     )
 
     if test_config is None:
-        #load the instance config, if it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
@@ -24,6 +24,7 @@ def create_app():
     except OSError:
         pass
 
+    # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
@@ -33,6 +34,11 @@ def create_app():
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    # imports and register the blog blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
